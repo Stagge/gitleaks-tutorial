9f0fda9137e5e920c788bb655cb84b4c2ac10a99
Stagge <jonatan.stagge@gmail.com>
2025-09-29 19:03:32 +0200

    Git config

--- gitleaks-tutorial/step2/background.sh
+++ gitleaks-tutorial/step2/background.sh
@@ -3,6 +3,9 @@
 mkdir -p ~/demo-repo && cd ~/demo-repo
 git init -q
 
+git config user.name "Demo User"
+git config user.email "demo@example.com"
+
 # Copy first file and commit
 wget https://raw.githubusercontent.com/Stagge/gitleaks-tutorial/refs/heads/main/gitleaks-tutorial/assets/demo-repo/app.py
 git add app.py
