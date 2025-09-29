0c2891c9e86e468db90e69d513b07f22eb540eeb
Stagge <jonatan.stagge@gmail.com>
2025-09-29 18:59:22 +0200

    Fix download

--- gitleaks-tutorial/step2/background.sh
+++ gitleaks-tutorial/step2/background.sh
@@ -4,11 +4,11 @@ mkdir -p ~/demo-repo && cd ~/demo-repo
 git init -q
 
 # Copy first file and commit
-cp /root/scenario-assets/demo-repo/app.py ~/demo-repo/app.py
+wget https://raw.githubusercontent.com/Stagge/gitleaks-tutorial/refs/heads/main/gitleaks-tutorial/assets/demo-repo/app.py
 git add app.py
 git commit -m "Initial commit" >/dev/null 2>&1
 
 # Copy second file and commit
-cp /root/scenario-assets/demo-repo/api.py ~/demo-repo/api.py
+wget https://raw.githubusercontent.com/Stagge/gitleaks-tutorial/refs/heads/main/gitleaks-tutorial/assets/demo-repo/api.py
 git add api.py
 git commit -m "Add API file" >/dev/null 2>&1
