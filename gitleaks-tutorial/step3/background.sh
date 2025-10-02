37f125fb69c14279fa2e01eba31cbd25fa18c1b0
Guillaume Roboam <guillaume_roboam@hotmail.fr>
2025-10-02 18:36:59 +0200

    fix: update go version for gitleaks

--- gitleaks-tutorial/step3/background.sh
+++ gitleaks-tutorial/step3/background.sh
@@ -3,6 +3,12 @@
 
 apt install python3.12-venv -y
 
+# update go version
+wget -q https://golang.org/dl/go1.23.2.linux-386.tar.gz
+tar -C /usr/local -xzf go1.23.2.linux-386.tar.gz
+ln -s /usr/local/go/bin/go /usr/bin/go
+rm go1.23.2.linux-386.tar.gz
+
 cd ~/demo-repo
 
 # Copy file and commit
